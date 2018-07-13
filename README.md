2a13dab9f05bdcc22260ca08a237df382617277c
EvgeniyGordinskiy <evgeniy@gordinskiy.com>
2018-07-14 00:26:19 +0300

    Added styles for html,body and app-root.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Tournament
+# crm_schools_frontend
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.0.8.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -22,6 +22,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to tournament!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to crm_schools_frontend!');
   }));
 });
